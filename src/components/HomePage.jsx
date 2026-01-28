261cc071763831707ee4af40cffd038506c1a19c
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-01-28 14:13:50 +0530

    mobile

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Sidebar from "./Home Comp/Sidebar";
 import MessageContainer from "./Messages/MessageContainer";
 import Home from "./Home";
-import useConversation from "../Zustand/useConversation"; // Zustand store import karein
+import useConversation from "../components/Zustand/useConversation"; // Zustand store import karein
 
 function HomePage() {
   const { selectedConversation } = useConversation();
@@ -13,7 +13,9 @@ function HomePage() {
         1. Agar chat selected hai toh Sidebar chhupa do (hidden), warna dikhao (flex).
         2. Tablet/Desktop (md breakpoint) par dono humesha dikhenge (md:flex).
       */}
-      <div className={`${selectedConversation ? "hidden" : "flex"} md:flex h-full w-full md:w-auto border-r border-[#313d45]/50`}>
+      <div
+        className={`${selectedConversation ? "hidden" : "flex"} md:flex h-full w-full md:w-auto border-r border-[#313d45]/50`}
+      >
         <Sidebar />
       </div>
 
@@ -21,7 +23,9 @@ function HomePage() {
         1. Agar chat selected NAHI hai toh MessageContainer chhupa do (hidden).
         2. Agar chat selected HAI toh full screen par dikhao (flex).
       */}
-      <div className={`${!selectedConversation ? "hidden" : "flex"} md:flex flex-1 h-full`}>
+      <div
+        className={`${!selectedConversation ? "hidden" : "flex"} md:flex flex-1 h-full`}
+      >
         <MessageContainer />
       </div>
     </Home>
