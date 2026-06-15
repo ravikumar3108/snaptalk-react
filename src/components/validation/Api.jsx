3bbc568efc9210c875b5bcab1602949b76819fc8
Ravi Kumar <raviluhaniwal318@gmail.com>
2026-06-15 11:20:34 +0530

    profile

--- src/components/validation/Api.jsx
+++ src/components/validation/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/";
-// const API_URL = "https://snaptalk-back.vercel.app";
+// const API_URL = "http://localhost:8000/";
+const API_URL = "https://snaptalk-back.vercel.app";
 function getToken() {
   return localStorage.getItem("token");
 }
